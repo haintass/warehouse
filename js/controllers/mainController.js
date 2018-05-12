914e248c05224756eace94ed338117549fd2dcc1
haintass <haintass@gmail.com>
2018-05-12 22:26:30 +0300

    menu
    change menu

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -3,17 +3,17 @@ var mainModule = angular.module("myApp", ['ngMessages']);
 mainModule.controller("mainCtrl", ["$scope", "memoryStorageRepositoryService", function($scope, memoryStorageRepositoryService) {
     $scope.menu = [
         {
-            name: "Склады",
+            name: "main page",
             value: "#1"
         },
         
         {
-            name: "Сотрудники",
+            name: "My warehouses",
             value: "#2"
         },
         
         {
-            name: "Товары",
+            name: "Goods",
             value: "#3"
         }
     ];
