38e5f16f5cb3f6fff4354ba937453c426a8bd0fa
haintass <haintass@gmail.com>
2018-05-12 22:19:47 +0300

    delete old code
    clean all

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -17,44 +17,6 @@ mainModule.controller("mainCtrl", ["$scope", "memoryStorageRepositoryService", f
             value: "#3"
         }
     ];
-    $scope.warehouses = {};
-    $scope.isShowWindowToCreateItem = false;
-    $scope.isNewItem = true;
-    $scope.item = {};
     
-    $scope.keyItem;
-    $scope.ToHideShowPopup = function(key) {
-        if (key == undefined) {
-            $scope.item = {};
-            $scope.isNewItem = true;
-            $scope.keyItem = key;
-        }
-        else {
-            $scope.isNewItem = false;
-            $scope.item = Object.assign({}, $scope.items[key]);    
-            $scope.keyItem = key;
-        }
-        
-        $scope.isShowWindowToCreateItem = !$scope.isShowWindowToCreateItem;
-    }
-     
-    $scope.isNewItem = memoryStorageRepositoryService.isNewItem;
-    
-    $scope.items = memoryStorageRepositoryService.GetItems();   
-    $scope.Add = memoryStorageRepositoryService.AddItem;
-    $scope.Change = memoryStorageRepositoryService.ChangeItem;
-    $scope.Delete = memoryStorageRepositoryService.DeleteItem;
-
-    $scope.AddChangeItem = function (action) {
-        if ($scope.addItemForm.$valid) {
-            action == 'add' ? $scope.Add() : $scope.Change();
-            $scope.ToHideShowPopup();
-        }
-    }
-    
-    $scope.DeleteItem = function() {
-        $scope.Delete();
-        $scope.ToHideShowPopup();
-    }
 }])
 
