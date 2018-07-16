6ba87cec20ca2dc96d9f82d2346cf0ba7a7ba57b
evgeny-kulakov <haintass@gmail.com>
2018-07-16 11:27:30 +0300

    logic of adding a fields of items
    move logic in directive controller

--- js/controllers/WarehouseController.js
+++ js/controllers/WarehouseController.js
@@ -4,17 +4,6 @@ angular.module("myApp").controller('WarehouseController',
         $scope.warehouse = memoryStorageRepositoryService.currentWarehouse;
         $scope.isShowPopup = false;
         $scope.isNewItem = true;
-        $scope.arr = [
-            { fieldName: "", fieldType: "" }
-        ]
-        
-        $scope.addField = function() {
-            $scope.arr.push({ fieldName: "", fieldType: "" });
-        }
-
-        $scope.removeField = function(index) {
-            $scope.arr.splice(index, 1);
-        }
 
         $scope.ToHideShowPopup = function() {
             $scope.isShowPopup = !$scope.isShowPopup;
--- js/directives/popupToCreateItem.js
+++ js/directives/popupToCreateItem.js
@@ -1,6 +1,17 @@
 angular.module("myApp").directive('popupToCreateItem', [function() {
     return {
         restrict: 'A',
-        templateUrl: 'templates/popups/popupToCreateItem.html'
+        templateUrl: 'templates/popups/popupToCreateItem.html',
+        controller: function($scope) {
+            $scope.itemFields = [{ fieldName: "", fieldType: "" }];
+            
+            $scope.addField = function() {
+                $scope.itemFields.push({ fieldName: "", fieldType: "" });
+            }
+            
+            $scope.removeField = function(index) {
+                $scope.itemFields.splice(index, 1);
+            }
+        }
     }
 }]);
